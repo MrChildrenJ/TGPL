96ef402ed41b8949702b42b21d290927294158f0
MrChildrenJ <jj.chunchieh.huang@gmail.com>
2025-08-20 20:47:04 -0600

    finish go tour

---
+++ GoTour/equivalentBinaryTree.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+
+func walkHelper(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	walkHelper(t.Left, ch)
+	ch <- t.Value
+	walkHelper(t.Right, ch)
+}
+
+func Walk(t *tree.Tree, ch chan int) {
+	defer close(ch) // can not put at the end!
+	walkHelper(t, ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+
+		if !ok1 && !ok2 {
+			return true
+		}
+
+		if ok1 != ok2 || v1 != v2 {
+			return false
+		}
+	}
+}
+
+func main() {
+	t1 := tree.New(1)
+	t2 := tree.New(1)
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for value := range ch1 {
+		fmt.Println(value)
+	}
+
+	for value := range ch2 {
+		fmt.Println(value)
+	}
+
+	fmt.Println(Same(t1, t2))
+
+}
